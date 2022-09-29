5ae41e416e50f1be804f67609d5b3793c171a6b8
student <student@DESKTOP-QRCOFTA>
2022-09-29 11:11:31 +0800

    Thymeleaf模板

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,24 +5,7 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
-
-	@GetMapping("/")
-	String home() {
-		return "Spring is here!";
-	}
-
-	@GetMapping("/hello")
-	Book hello(String name)
-	{
-		Book b = new Book();
-		b.ID = 1000;
-		b.Name = name;
-		return b;
-	}
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,13 +1,15 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-    @RequestMapping("/myhello")
-    String myhello()
+    @RequestMapping("/hello")
+    String hello(String username, Model model)
     {
+        model.addAttribute("username", username);
         return "hello";
     }
 
